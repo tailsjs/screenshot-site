fc004d8f17aa1dcca005bac9fedeff1e28322052
tailsjs <49571184+tailsjs@users.noreply.github.com>
2019-05-19 09:58:40 +0300

    Updated

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 module.exports.getUrl = function(x, y, format, newUrl){
-	if(format != "png" && format != "jpeg"){
-		throw new Error(`Ошибка! Неккоректный формат картинки!`)
-	}
-	if(isNaN(y)){
-		throw new Error(`Ошибка! Неккоректная ширина!`)
-	}
+	
 	if(!newUrl){
-		 throw new Error(`Ошибка! Не указан URL!`)
+		 throw new Error('Ошибка! Не указан URL!')
 	}
 	if(!x){
-		 throw new Error(`Ошибка! Не указан размер по коордитате XY!`)
+		 throw new Error('Ошибка! Не указан размер по коордитате XY!')
+	}
+	if(isNaN(y)){
+		throw new Error('Ошибка! Неккоректная ширина!')
 	}
 	if(!y){
-		 throw new Error(`Ошибка! Не указана ширина скрина!`)
+		 throw new Error('Ошибка! Не указана ширина скрина!')
+	}
+		if(format != "png" && format != "jpeg"){
+		throw new Error('Ошибка! Неккоректный формат картинки! Используются только: JPEG и PNG!')
 	}
 	if(!format){
-		format = `jpeg`
+		format = 'jpeg'
 	};
 	
-	let url = `http://mini.s-shot.ru/` + x + `/` + y + `/` + format + `/?` + newUrl;
+	let url = 'http://mini.s-shot.ru/' + x + '/' + y + '/' + format + '/?' + newUrl;
     return url;
 }
